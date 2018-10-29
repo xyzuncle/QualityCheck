93991b53f85a61c26d1f463fe906ff62410500f4
xyzuncle <w68335397@163.com>
2018-10-30 07:06:27 +0800

    Initial commit

---
+++ quality/src/main/java/com/quality/QualityApplication.java
@@ -0,0 +1,12 @@
+package com.quality;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QualityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QualityApplication.class, args);
+	}
+}
---
+++ quality/src/test/java/com/quality/QualityApplicationTests.java
@@ -0,0 +1,16 @@
+package com.quality;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class QualityApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

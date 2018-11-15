2feb594930b1b2216edf2bd2945086792a06b815
孙中伟 <sunzhongwei@163.com>
2018-11-15 13:56:17 +0800

    添加接口日志

---
+++ quality/src/main/java/com/quality/mapper/QualityLoggerMapper.java
@@ -0,0 +1,16 @@
+package com.quality.mapper;
+
+import com.quality.entity.QualityLogger;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author yerui
+ * @since 2018-11-13
+ */
+public interface QualityLoggerMapper extends BaseMapper<QualityLogger> {
+
+}

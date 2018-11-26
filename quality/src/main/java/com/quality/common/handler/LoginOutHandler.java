952ca8e70e3b49306d496048e287eea0d321fc8a
xyzuncle <w68335397@163.com>
2018-11-26 19:25:31 +0800

    退出登录 增加日志

--- quality/src/main/java/com/quality/common/handler/LoginOutHandler.java
+++ quality/src/main/java/com/quality/common/handler/LoginOutHandler.java
@@ -2,6 +2,7 @@ package com.quality.common.handler;
 
 import com.alibaba.fastjson.JSON;
 
+import com.quality.common.aop.WebLogAction;
 import com.quality.common.dto.ObjectRestResponse;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
@@ -15,8 +16,10 @@ import java.io.IOException;
 /**
  * 成功登出后需要执行的行为
  */
+
 public class LoginOutHandler implements LogoutSuccessHandler {
     @Override
+    @WebLogAction(moduleName = "退出登录",operation = "用户退出系统",operation_type = "退出登录")
     public void onLogoutSuccess(HttpServletRequest httpServletRequest, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
       /*  String userName = authentication.getName().toString();*/
         String result = JSON.toJSONString(new ObjectRestResponse().data("true").message("用户退出！"));

c99b01fc94cfae3eaf911a6a3eb01d0d51e2bf5c
xyzuncle <w68335397@163.com>
2018-12-02 10:57:06 +0800

    解决activity 的security配置和 spring boot配置重复， 同时解决 activity 默认用的ibatis 和该项目的mybaits plus重复的问题

--- quality/src/main/java/com/quality/QualityApplication.java
+++ quality/src/main/java/com/quality/QualityApplication.java
@@ -1,9 +1,10 @@
 package com.quality;
 
+import org.activiti.spring.boot.SecurityAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class QualityApplication {
 
 	public static void main(String[] args) {

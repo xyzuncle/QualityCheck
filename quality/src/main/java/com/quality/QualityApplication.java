7b874773ba0afdc7046d5372429d474c63f4ba03
xyzuncle <w68335397@163.com>
2018-12-05 09:54:50 +0800

    flowable 包和 配置文件

--- quality/src/main/java/com/quality/QualityApplication.java
+++ quality/src/main/java/com/quality/QualityApplication.java
@@ -1,6 +1,7 @@
 package com.quality;
 
-import org.activiti.spring.boot.SecurityAutoConfiguration;
+
+import org.flowable.spring.boot.SecurityAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
